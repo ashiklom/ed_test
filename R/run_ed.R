9e571cb8ff5e1c69503d2f5264e1c3f497d3c7ad
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-04-26 15:11:54 -0400

    Bugfix in run_ed

--- R/run_ed.R
+++ R/run_ed.R
@@ -1,10 +1,10 @@
-run_ed <- function(ed2in) {
+run_ed <- function(ed2in, ...) {
   outfile <- here::here("input", "ED2IN")
   if (fs::file_exists(outfile)) {
     fs::file_delete(outfile)
   }
   write_ed2in(ed2in, outfile, barebones = TRUE)
-  prun_ed()
+  prun_ed(...)
 }
 
 prun_ed <- function(...) {
